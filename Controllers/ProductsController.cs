f512b142100f018eccf2cd7d039865c25d925a85
Sazib204061 <mdsaziburrahman440@gmail.com>
2025-03-10 19:59:22 +0600

    Implement Add new option

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using BasicCRUDOperation.Data;
 using BasicCRUDOperation.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BasicCRUDOperation.Controllers
 {
@@ -11,10 +12,34 @@ namespace BasicCRUDOperation.Controllers
         {
             _db = db;   //_dp ar modde sql connection store kore nilam and ata use kore database er data access korbo
         }
+
+        /*
         public IActionResult Index()
         {
             List<Product> products = _db.Products.ToList();  //Get all the products from the database
             return View(products);   //send products list to index.cshtml file
+        }
+        */
+        //using asynchronous programming
+        public async Task<IActionResult> Index()
+        {
+            var products = await _db.Products.ToListAsync(); // Fetch all products
+            return View(products);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            await _db.Products.AddAsync(product);  //Asynchronusly add all data to Products table
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");  //here we stay same controller that's why does not need to specify controller name
+            //return RedirectToAction("Action_Name", "Controller_Name");
         }
     }
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,9 +1,18 @@
-﻿namespace BasicCRUDOperation.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BasicCRUDOperation.Models
 {
     public class Product
     {
+        //[Key]
         public int Id { get; set; }
+        //here we abel to add diffrent type of data anotation ex- [Required], [Key]
+        //[Required]
+        //[DisplayName("Product Name")] //using this we set form label name
         public string Name { get; set; }
+        //[DisplayName("Product Price")]
+
         public decimal Price { get; set; }
     }
 }
