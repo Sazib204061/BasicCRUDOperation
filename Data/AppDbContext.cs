0f0466d83af7549274898200c2d4dbf6d70c8911
Sazib204061 <mdsaziburrahman440@gmail.com>
2025-03-10 16:08:12 +0600

    Add Migrations and Initially Seed some data into Products table.

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -8,5 +8,30 @@ namespace BasicCRUDOperation.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Product> Products { get; set; }
+
+
+        //here we are seeding the data or adding the data to the database
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product {
+                    Id = 1,
+                    Name = "Samsung",
+                    Price = 100
+                },
+                new Product
+                {
+                    Id = 2,
+                    Name = "IPhone",
+                    Price = 200
+                },
+                new Product
+                {
+                    Id = 3,
+                    Name = "Appo",
+                    Price = 150
+                }
+            );
+        }
     }
 }
---
+++ Migrations/20250310100434_SeedProductsTable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BasicCRUDOperation.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedProductsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "Samsung", 100m },
+                    { 2, "IPhone", 200m },
+                    { 3, "Appo", 150m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
